2a64744ebe5215ffaf03f1431153ead4b4d1ac7e
ik <ik@callibri.ru>
2018-08-31 10:34:07 +0500

    ADD

--- add/js/common.js
+++ add/js/common.js
@@ -26,7 +26,7 @@ $('#problems-slider').slick({
         breakpoint: 1024,
         settings: { slidesToShow: 2, slidesToScroll: 2, infinite: false, dots: true }
     }, {
-        breakpoint: 600,
+        breakpoint: 769,
         settings: { slidesToShow: 1, slidesToScroll: 1 }
     }, {
         breakpoint: 480,
