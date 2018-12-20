678c09a5f774c1d590a6e9558433badf69dc3191
ne31oi <ne31oi@gmail.com>
2018-12-20 12:02:58 +0500

    +

---
+++ micro/src/main.js
@@ -0,0 +1,141 @@
+var rect = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 55 16" with="55" height="16"><rect class="strokedrect3" x="0" y="0" width="55" height="16" /></svg>',
+    scheme_svg = '<?xml version="1.0" encoding="UTF-8"?><svg enable-background="new 0 0 512 512" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="m54.857 110.24c-5.77 0-10.449 4.679-10.449 10.449v46.498h-33.959c-5.77 0-10.449 4.679-10.449 10.449s4.679 10.449 10.449 10.449h33.959v31.347h-33.959c-5.77 0-10.449 4.679-10.449 10.449s4.679 10.449 10.449 10.449h33.959v31.347h-33.959c-5.77-1e-3 -10.449 4.679-10.449 10.448 0 5.77 4.679 10.449 10.449 10.449h33.959v31.347h-33.959c-5.77 0-10.449 4.679-10.449 10.449s4.679 10.449 10.449 10.449h33.959v46.498c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-270.63c0-5.77-4.679-10.449-10.449-10.449z"/><path d="m391.31 446.69h-270.63c-5.77 0-10.449 4.679-10.449 10.449s4.679 10.449 10.449 10.449h46.498v33.959c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-33.959h31.347v33.959c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-33.959h31.347v33.959c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-33.959h31.347v33.959c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-33.959h46.498c5.77 0 10.449-4.679 10.449-10.449-1e-3 -5.77-4.68-10.449-10.45-10.449z"/><path d="m120.69 65.306h270.63c5.77 0 10.449-4.679 10.449-10.449s-4.679-10.449-10.449-10.449h-46.498v-33.959c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v33.959h-31.347v-33.959c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v33.959h-31.347v-33.959c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v33.959h-31.347v-33.959c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v33.959h-46.498c-5.77 0-10.449 4.679-10.449 10.449s4.679 10.449 10.449 10.449z"/><path d="m391.31 86.204h-270.63c-19.013 0-34.482 15.469-34.482 34.482v270.63c0 19.013 15.469 34.482 34.482 34.482h270.63c19.013 0 34.482-15.469 34.482-34.482v-270.63c-1e-3 -19.013-15.47-34.482-34.483-34.482zm13.584 305.11c0 7.49-6.094 13.584-13.584 13.584h-270.63c-7.49 0-13.584-6.094-13.584-13.584v-270.63c0-7.49 6.094-13.584 13.584-13.584h270.63c7.49 0 13.584 6.094 13.584 13.584v270.63z"/><path d="m373.55 128h-235.1c-5.77 0-10.449 4.679-10.449 10.449v235.1c0 5.77 4.679 10.449 10.449 10.449h235.1c5.77 0 10.449-4.679 10.449-10.449v-61.127c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v50.678h-214.2v-214.2h214.2v32.914c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-43.363c0-5.77-4.679-10.449-10.449-10.449z"/><path d="m373.55 238.68c-5.77 0-10.449 4.679-10.449 10.449v1.045c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-1.045c0-5.771-4.679-10.449-10.449-10.449z"/><path d="m501.55 240.33c5.77 0 10.449-4.679 10.449-10.449s-4.679-10.449-10.449-10.449h-33.959v-31.347h33.959c5.77 0 10.449-4.679 10.449-10.449s-4.679-10.449-10.449-10.449h-33.959v-46.498c0-5.77-4.679-10.449-10.449-10.449s-10.449 4.679-10.449 10.449v270.63c0 5.77 4.679 10.449 10.449 10.449s10.449-4.679 10.449-10.449v-46.498h33.959c5.77 0 10.449-4.679 10.449-10.449s-4.679-10.449-10.449-10.449h-33.959v-31.347h33.959c5.77 0 10.449-4.679 10.449-10.449s-4.679-10.449-10.449-10.449h-33.959v-31.347h33.959z"/><circle cx="186.38" cy="186.38" r="10.449"/><circle cx="325.61" cy="325.61" r="10.449"/></svg>',
+    phone_svg = '<?xml version="1.0" encoding="UTF-8"?><svg enable-background="new 0 0 489.592 489.592" version="1.1" viewBox="0 0 489.592 489.592" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="m282 0.296h-238.3c-23.9 0-43.7 19.8-43.7 43.7v401.6c0 23.9 19.8 43.7 43.7 43.7h237.2c25 0 43.7-19.8 44.7-43.7v-401.6c0.1-23.9-19.7-43.7-43.6-43.7zm3.1 40.6v295.5h-244.5v-295.5h244.5zm0 407.8h-244.5v-70.7h244.5v70.7z"/><path d="m179 393.6h-33.3c-11.4 0-20.8 9.4-20.8 20.8s9.4 20.8 20.8 20.8h33.3c11.4 0 19.8-9.4 20.8-20.8 0-11.5-9.4-20.8-20.8-20.8z"/><path d="m372.5 184.5c-10.4 4.2-15.6 15.6-12.5 26 0 1 13.5 42.7 0 91.5-3.1 10.4 3.1 21.8 13.5 25 13 3.7 22.9-5.2 25-14.6 17.7-60.3 1-113.4 0-115.5-4.2-10.4-15.6-15.6-26-12.4z"/><path d="m475.5 154.3c-4.2-10.4-15.6-15.6-26-12.5-10.4 4.2-15.6 15.6-12.5 26 0 1 27 84.3 0 176.9-3.1 10.4 3.1 21.8 13.5 25 13.1 3.8 21.8-5.2 25-14.6 31.2-106.1 1-196.6 0-200.8z"/></svg>',
+    cloud_svg = '<?xml version="1.0" encoding="UTF-8"?><svg enable-background="new 0 0 491.2 491.2" version="1.1" viewBox="0 0 491.2 491.2" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">		<path d="m432.6 163.2c-4.2-54.2-49-98-103.2-98-14.6 0-29.2 2.1-41.7 8.3-27.1-44.8-74-73-127.2-73-82.3 0.1-149.1 67.9-149.1 152.3 0 14.6 2.1 29.2 6.3 43.8-11.5 15.6-17.7 35.4-17.7 55.3 0 53.2 42.7 97 94.9 97h11.5v120.9c0 11.5 9.4 20.9 20.9 20.9h217.9c10.4 0 19.8-9.4 20.9-20.9v-119.9h29.2c52.1 0 94.9-43.8 95.9-97-0.2-39.6-24.2-75.1-58.6-89.7zm-108.4 286.7h-176.2v-128.2h176.2v128.2zm-133.4-169.9v-44.8c0-25 19.8-44.8 44.8-44.8s44.8 20.9 44.8 44.8v44.8h-89.6zm204.3 28.2h-29.2v-7.3c0-11.5-9.4-20.9-20.9-20.9h-24v-44.8c0-46.9-38.6-85.5-85.5-85.5s-85.5 38.6-85.5 85.5v44.8h-22.9c-11.5 0-20.9 9.4-20.9 20.9v6.3h-12.5c-30.2 0-54.2-25-54.2-56.3 0-14.6 5.2-28.1 14.6-38.6 5.2-5.2 7.3-13.6 4.2-20.9-5.2-12.5-7.3-27.1-7.3-40.7 0-61.5 49-111.6 108.4-111.6 43.8 0 83.4 27.1 100.1 68.8 2.9 10.3 18.7 19.3 31.3 9.4 11.5-8.3 24-12.5 37.5-12.5 34.4 0 62.7 29.2 62.6 64.6-0.1 17.6 2.8 23.3 15.6 27.1 25 6.3 42.7 29.2 42.7 55.3 0.1 31.3-23.9 56.4-54.1 56.4z"/>		<path d="m235.6 434.3c11.5 0 20.9-9.4 20.9-20.9v-28.1c0-11.5-9.4-20.9-20.9-20.9s-20.9 9.4-20.9 20.9v28.1c0 11.5 9.4 20.9 20.9 20.9z"/></svg>',
+    lines = [document.getElementById('lines1'), document.getElementById('lines2'), document.getElementById('lines3')],
+    step = 1,
+    active = false,
+    scheme1 = document.getElementById('micro_scheme1'),
+    scheme2 = document.getElementById('micro_scheme2'),
+    phone = document.getElementById('micro_phone'),
+    cloud = document.getElementById('micro_cloud');
+scheme1.innerHTML = scheme_svg;
+scheme2.innerHTML = scheme_svg;
+phone.innerHTML = phone_svg;
+cloud.innerHTML = cloud_svg;
+
+lines.forEach(function(line) {
+    line.innerHTML = rect;
+})
+
+function timeActive(time) {
+    setTimeout(function() { active = false }, time)
+
+}
+
+function next() {
+    if (step < 9 && !active) {
+        switch (step) {
+            case 1:
+                active = true;
+                cloud.classList.remove('micro_opacity');
+                timeActive(500);
+                break;
+            case 2:
+                active = true;
+                scheme1.classList.add('green');
+                setTimeout(function() { scheme1.classList.remove('micro_opacity'); }, 500)
+                timeActive(500);
+                break;
+            case 3:
+                active = true;
+                scheme1.classList.add('shake');
+                timeActive(500);
+                break;
+            case 4:
+                active = true;
+                scheme1.classList.remove('shake');
+                scheme2.classList.add('green');
+                setTimeout(function() { scheme2.classList.remove('micro_opacity'); }, 500)
+                timeActive(500);
+                break;
+            case 5:
+                active = true;
+                lines[0].classList.remove('micro_hide');
+                timeActive(500);
+                break;
+            case 6:
+                active = true;
+                scheme1.classList.add('red');
+                lines[0].classList.add('micro_hide');
+                timeActive(500);
+                break;
+            case 7:
+                active = true;
+                phone.classList.add('green');
+                setTimeout(function() {
+                    phone.classList.remove('micro_opacity');
+                    lines[1].classList.remove('micro_hide');
+                }, 500)
+
+                timeActive(500);
+                break;
+            case 8:
+                active = true;
+                lines[1].getElementsByClassName('strokedrect3')[0].classList.add('noanim');
+                lines[2].classList.remove('micro_hide');
+                timeActive(500);
+                break;
+        }
+        step++;
+    }
+}
+
+function prev() {
+    if (step > 1 && !active) {
+        switch (step) {
+            case 2:
+                active = true;
+                cloud.classList.add('micro_opacity');
+                timeActive(500);
+                break;
+            case 3:
+                active = true;
+                scheme1.classList.remove('green');
+                setTimeout(function() { scheme1.classList.add('micro_opacity'); }, 500)
+                timeActive(500);
+                break;
+            case 4:
+                active = true;
+                scheme1.classList.add('shake');
+                timeActive(500);
+                break;
+            case 5:
+                active = true;
+                scheme1.classList.remove('shake');
+                scheme2.classList.remove('green');
+                setTimeout(function() { scheme2.classList.add('micro_opacity'); }, 500)
+                timeActive(500);
+                break;
+            case 6:
+                active = true;
+                lines[0].classList.add('micro_hide');
+                timeActive(500);
+                break;
+            case 7:
+                active = true;
+                scheme1.classList.remove('red');
+                lines[0].classList.remove('micro_hide');
+                timeActive(500);
+                break;
+            case 8:
+                active = true;
+                phone.classList.remove('green');
+                setTimeout(function() {
+                    phone.classList.add('micro_opacity');
+                    lines[1].classList.add('micro_hide');
+                }, 500)
+
+                timeActive(500);
+                break;
+            case 9:
+                active = true;
+                lines[1].getElementsByClassName('strokedrect3')[0].classList.remove('noanim');
+                lines[2].classList.add('micro_hide');
+                timeActive(500);
+                break;
+
+        }
+        step--;
+    }
+}
