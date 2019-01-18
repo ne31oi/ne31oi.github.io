a531cf9cfdcdcfca7b365d7b55574e567d33f6e9
ne31oi <ne31oi@gmail.com>
2019-01-18 14:21:20 +0500

    +

--- TaxBuro/mail.php
+++ TaxBuro/mail.php
@@ -2,11 +2,11 @@
         // $_POST['title'] содержит данные из поля "Тема"
                 $title = 'TEST';
 
-                $mess =  $_POST['name']\n$_POST['email']\n$_POST['msg'];
+                $mess =  "$_POST['name']\n$_POST['email']\n$_POST['msg']";
         // $to - кому отправляем
-                $to = 'mail@gmail.com';
+                $to = 'taxburo@protonmail.com';
         // $from - от кого
-                $from='mail@gmail.com';
+                $from='taxburo@protonmail.com';
         // функция, которая отправляет наше письмо.
                 mail($to, $title, $mess, 'from:'.$from, '-f'.$from);
                 echo 'Спасибо! Ваше письмо отправлено.'; ?>
